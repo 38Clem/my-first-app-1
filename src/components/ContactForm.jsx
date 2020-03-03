1123561b630ba60addb0503f9e56ac23d821277b
Clemence <38Clem@users.noreply.github.com>
2020-03-03 14:27:17 +0100

    Add state and onChange event

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
-const ContactForm = () =>
-  <Form>
-    <Form.Group controlId="formBasicEmail">
-      <Form.Label>Email address</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
-      <Form.Text className="text-muted">
-        We'll never share your email with anyone else.
-      </Form.Text>
-    </Form.Group>
+class ContactForm extends Component {
 
-    <Form.Group controlId="formBasicPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-    <Form.Group controlId="formBasicCheckbox">
-      <Form.Check type="checkbox" label="Check me out" />
-    </Form.Group>
-    <Button variant="primary" type="submit">
-      Submit
+  state = {
+    email: '',
+    message:'',
+  };
+
+  handleEmailChange = (event) => {
+    const email = event.target.value
+    this.setState({email})
+    console.log(email);
+    
+  }
+
+  render = () => {
+    return (
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" onChange={this.handleEmailChange}/>
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+      </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicMessage">
+          <Form.Label>Your Message</Form.Label>
+          <Form.Control as="textarea" rows="3" placeholder="Type your message here" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Send
     </Button>
-  </Form>
-;
+      </Form>
+
+    )
+
+  }
+};
 
 export default ContactForm;
